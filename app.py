5ef621c3629953f927f28eed54aaa70c24977d2d
Ruilin <97988939+CChuxue@users.noreply.github.com>
2022-03-25 22:19:00 -0400

    update app

--- app.py
+++ app.py
@@ -1,25 +1,109 @@
+# -*- coding: utf-8 -*-
+
+"""
+Chat Server
+===========
+
+This simple application uses WebSockets to run a primitive chat server.
+"""
+
+import os
+import logging
+import redis
+import gevent
+from flask import Flask, render_template
+from flask_sockets import Sockets
 from flask import Flask, render_template
 
+# REDIS_URL = os.environ['REDISTOGO_URL', 'redis://localhost:8000']
+REDIS_CHAN = 'chat'
+
+# redis://redistogo:bf2886f0eaf59929fd616105d69e12d1@sole.redistogo.com:10615/
+REDISTOGO_URL = os.getenv('REDISTOGO_URL', None)
+if REDISTOGO_URL is None:
+    REDIS_URL = '127.0.0.1:8000'
+else:
+    REDIS_URL = REDISTOGO_URL
+    REDIS_URL = REDIS_URL.split('redis://redistogo:')[1]
+    REDIS_URL = REDIS_URL.split('/')[0]
+    REDIS_PWD, REDIS_HOST = REDIS_URL.split('@', 1)
+    REDIS_URL = "%s?password=%s" % (REDIS_HOST, REDIS_PWD)
+session_opts = {'session.type': 'redis', 'session.url': REDIS_URL, 'session.data_dir': './cache/',
+                'session.key': 'appname', 'session.auto': True, }
+
 app = Flask(__name__)
+app.debug = 'DEBUG' in os.environ
 
-@app.route("/")
-def index():
+sockets = Sockets(app)
+redis = redis.from_url(REDIS_URL)
 
-    # Load current count
-    f = open("count.txt", "r")
-    count = int(f.read())
-    f.close()
 
-    # Increment the count
-    count += 1
+class ChatBackend(object):
+    """Interface for registering and updating WebSocket clients."""
 
-    # Overwrite the count
-    f = open("count.txt", "w")
-    f.write(str(count))
-    f.close()
+    def __init__(self):
+        self.clients = list()
+        self.pubsub = redis.pubsub()
+        self.pubsub.subscribe(REDIS_CHAN)
 
-    # Render HTML with count variable
-    return render_template("index.html", count=count)
+    def __iter_data(self):
+        for message in self.pubsub.listen():
+            data = message.get('data')
+            if message['type'] == 'message':
+                app.logger.info(u'Sending message: {}'.format(data))
+                yield data
 
-if __name__ == "__main__":
-    app.run()
+    def register(self, client):
+        """Register a WebSocket connection for Redis updates."""
+        self.clients.append(client)
+
+    def send(self, client, data):
+        """Send given data to the registered client.
+        Automatically discards invalid connections."""
+        try:
+            client.send(data)
+        except Exception:
+            self.clients.remove(client)
+
+    def run(self):
+        """Listens for new messages in Redis, and sends them to clients."""
+        for data in self.__iter_data():
+            for client in self.clients:
+                gevent.spawn(self.send, client, data)
+
+    def start(self):
+        """Maintains Redis subscription in the background."""
+        gevent.spawn(self.run)
+
+
+chats = ChatBackend()
+chats.start()
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@sockets.route('/submit')
+def inbox(ws):
+    """Receives incoming chat messages, inserts them into Redis."""
+    while not ws.closed:
+        # Sleep to prevent *constant* context-switches.
+        gevent.sleep(0.1)
+        message = ws.receive()
+
+        if message:
+            app.logger.info(u'Inserting message: {}'.format(message))
+            redis.publish(REDIS_CHAN, message)
+
+
+@sockets.route('/receive')
+def outbox(ws):
+    """Sends outgoing chat messages, via `ChatBackend`."""
+    chats.register(ws)
+
+    while not ws.closed:
+        # Context switch while `ChatBackend.start` is running in the background.
+        gevent.sleep(0.1)
+
